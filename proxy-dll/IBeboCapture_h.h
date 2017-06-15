4a516cca700a11c8db5aa0dfc2fbcecaa68a2f3d
Jake Loo <slow-@live.com>
2017-06-14 18:46:17 -0700

    v0.5.6

--- proxy-dll/IBeboCapture_h.h
+++ proxy-dll/IBeboCapture_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Wed Jun 14 17:05:50 2017
+/* at Wed Jun 14 18:45:21 2017
  */
 /* Compiler settings for ..\bebo-capture-svc\IBeboCapture.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.00.0603 
