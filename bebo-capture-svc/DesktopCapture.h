79db3fc470d02c68d79da9ff5dd3f9f08279bd19
Jake Loo <slow-@live.com>
2017-06-16 00:09:36 -0700

    print error, prevent infinite initializion

--- bebo-capture-svc/DesktopCapture.h
+++ bebo-capture-svc/DesktopCapture.h
@@ -7,47 +7,6 @@
 #include <stdint.h>
 #include "CommonTypes.h"
 
-class Rect {
-public:
-	Rect() : _left(0), _top(0), _right(0), _bottom(0) {}
-
-	static Rect MakeWH(int width, int height) {	
-		return Rect(0, 0, width, height);
-
-	}
-	static Rect MakeXYWH(int x, int y, int width, int height) {
-		return Rect(x, y, x + width, y + height);
-	}
-
-	static Rect MakeLTRB(int left, int top, int right, int bottom) {	
-		return Rect(left, top, right, bottom);
-	}
-
-	int left() { return _left;  }
-	int top() { return _top;  }
-	int right() { return _right;  }
-	int bottom() { return _bottom;  }
-	int width() { return _right - _left;  }
-	int height() { return _bottom - _top;  }
-	int x() { return _left; }
-	int y() { return _top; }
-
-	void translate(int dx, int dy) {
-		_left += dx;
-		_top += dy;
-		_right += dx;
-		_bottom += dy;
-	}
-
-private:
-	Rect(int left, int top, int right, int bottom): _left(left), _top(top), _right(right), _bottom(bottom) {}
-
-	int _left;
-	int _top;
-	int _right;
-	int _bottom;
-};
-
 class DesktopFrame {
 public:
 	DesktopFrame(int width, int height, int stride, uint8_t* const data) :
@@ -75,10 +34,12 @@ public:
 	bool GetOldFrame(IMediaSample *pSimple, int width, int height, bool captureMouse);
 	bool DoneWithFrame();
 	bool IsReady() { return m_Initialized;  };
-
+	bool ExceedMaxRetry() { return m_InitializeFailCount >= MAX_INIT_RETRY_COUNT;  };
 
 private:
 	// methods
+	static const int MAX_INIT_RETRY_COUNT = 20;
+
 	void ProcessFrame(FrameData * Data, int OffsetX, int OffsetY);
 	void CopyDirty(FrameData* Data, INT OffsetX, INT OffsetY);
 	void CopyMove(FrameData* Data, INT OffsetX, INT OffsetY);
@@ -88,8 +49,7 @@ private:
 	void SetMoveRect(_Out_ RECT* SrcRect, _Out_ RECT* DestRect, _In_ DXGI_OUTPUT_DESC* DeskDesc, _In_ DXGI_OUTDUPL_MOVE_RECT* MoveRect, INT TexWidth, INT TexHeight);
 
 	HRESULT InitializeDXResources();
-	HRESULT CreateSurface();
-	HRESULT InitDupl();
+	HRESULT InitDuplication();
 	HRESULT ReinitializeDuplication();
 
 	bool AcquireNextFrame(DXGI_OUTDUPL_FRAME_INFO * frame);
@@ -111,6 +71,7 @@ private:
 	int m_iAdapterNumber;
 	int m_iDesktopNumber;
 	bool m_Initialized;
+	int m_InitializeFailCount;
 	FrameData* m_LastFrameData;
 	DesktopFrame* m_LastDesktopFrame;
 
