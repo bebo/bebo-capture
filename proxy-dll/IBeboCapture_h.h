15759c8b927d6d49f9949c8f926fee2995770091
Jake Loo <slow-@live.com>
2017-08-30 14:50:45 -0700

    Bump version

--- proxy-dll/IBeboCapture_h.h
+++ proxy-dll/IBeboCapture_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Tue Aug 29 23:02:30 2017
+/* at Wed Aug 30 14:02:11 2017
  */
 /* Compiler settings for ..\bebo-capture-svc\IBeboCapture.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 8.00.0603 
