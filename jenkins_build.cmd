3eb332523624b8d27bd7f61a9fba4e9fd43ff695
Florian P. Nierhaus <fpn@bebo.com>
2018-02-20 16:42:20 -0800

    try to fix jenkins build, lets see whether that worked

--- jenkins_build.cmd
+++ jenkins_build.cmd
@@ -1,10 +1,45 @@
-"C:\Program Files (x86)\MSBuild\14.0\bin\MSBuild.exe" /property:Configuration=Release /property:Platform=x64 /target:clean,build
+ECHO ON
+set errorlevel=
+
+rmdir /s /q dist
+rmdir /s /q x64
+rmdir /s /q Release
+
+pushd .
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat" amd64
+popd
+MSBuild.exe /property:Configuration=Release /property:Platform=x64 /target:clean,build
+
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
 
 mkdir dist
-copy x64\Release\gst-to-dshow.DLL dist
-copy x64\Release\libgstdshowfiltersink.dll dist
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+xcopy x64\Release\*.dll dist\
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+xcopy x64\Release\*.pdb dist\
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+xcopy third_party\obs-binaries\* dist\
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
 
 set FILENAME=bebo-capture_%TAG%.zip
+
 "C:\Program Files\7-Zip\7z.exe" a -r %FILENAME% -w .\dist\* -mem=AES256
 
-"C:\Program Files\Amazon\AWSCLI\aws.exe" s3api put-object --bucket bebo-app --key repo/bebo-capture/%FILENAME% --body %FILENAME%
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+"C:\Program Files\Amazon\AWSCLI\aws.exe" s3api put-object --bucket bebo-app --key repo/bebo-capture/win64/%FILENAME% --body %FILENAME%
